327523ac48074945fd9b7bfc7cd4b34a4cdfdeaf
dnwls0612 <dnwls0612@naver.com>
2017-12-08 16:18:14 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#ifndef Header_Include
+	#define Header_Include
+	#include "header.h"
+#endif
+
+int main(){
+	AdminMovie * AM = (AdminMovie *)malloc(sizeof(AdminMovie));
+	AdminActor * AA = (AdminActor *)malloc(sizeof(AdminActor));
+	AdminDirector * AD = (AdminDirector *)malloc(sizeof(AdminDirector));
+	void (*fptr)(int) = ControlC;
+	StartProgram(AM, AD, AA);
+	signal(SIGINT, fptr);
+	while(1)
+		CommandInput(AM, AD, AA);
+	
+	return 0;
+}
+
+void ControlC(int sig){
+	char ch;
+	printf("Get Interrupt Signal\n");
+	printf("Do you want to exit myMovie program? (Y/N) ");
+	ch = getchar();
+	if(ch == 'Y' || ch == 'y')
+		exit(0);
+	else if(ch == 'N' || ch == 'n')
+		printf("(movie) ");
+}
