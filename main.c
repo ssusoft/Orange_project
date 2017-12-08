765decb954322f884ec261530d3e0d91be820b5e
dnwls0612 <dnwls0612@naver.com>
2017-12-08 21:23:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,7 @@ int main(){
 	AdminActor * AA = (AdminActor *)malloc(sizeof(AdminActor));
 	AdminDirector * AD = (AdminDirector *)malloc(sizeof(AdminDirector));
 	void (*fptr)(int) = ControlC;
-	StartProgram(AM, AD, AA);
-	signal(SIGINT, fptr);
-	while(1)
-		CommandInput(AM, AD, AA);
+	
 	
 	return 0;
 }
